94a2e1bf9a2c59583c58885d06cedc476c11d4ee
Anil Kerai <kerai.anil@googlemail.com>
2023-02-13 16:01:14 +0000

    converting to string extensions

---
+++ text-formatter/StringExtensions.cs
@@ -0,0 +1,51 @@
+namespace text_formatter;
+
+public static class StringExtensions
+{
+    public static IEnumerable<string> GetLines(this string text)
+    {
+        var lines =
+                text
+                    .Split(',', '.')
+                    .Where(s => !string.IsNullOrEmpty(s))
+                    .ToList()
+            ;
+        return lines;
+    }
+
+    public static string GetPaddedLine(this string line, int largestWordLength)
+    {
+        var splitWords =
+                line
+                    .Split('$')
+                    .Where(s => !string.IsNullOrEmpty(s))
+                    .ToList()
+            ;
+        
+        var paddedWords = 
+                splitWords
+                    .Select(s => s.GetPaddedWord(largestWordLength))
+                    .ToList()
+            ;
+        
+        paddedWords.Add("\n");
+        
+        return string.Concat(paddedWords);
+    }
+
+    public static string GetPaddedWord(this string word, int largestWordLength)
+    {
+        var numberOfPaddingChars = largestWordLength - word.Length;
+        var paddingWord = string.Concat(Enumerable.Repeat(' ', numberOfPaddingChars));
+        return word + paddingWord;
+    }
+    
+    public static int GetLengthOfLargestWordInString(this string text)
+    {
+        return text
+                .Split('$')
+                .Select(s => s.Replace(",", string.Empty))
+                .Max(s => s.Length)
+            ;
+    }
+}
--- text-formatter/TextFormatter.cs
+++ text-formatter/TextFormatter.cs
@@ -2,67 +2,21 @@ namespace text_formatter;
 
 public static class TextFormatter
 {
+    private const int MIN_PADDING = 2;
     public static string Format(string text)
     {
         if (string.IsNullOrEmpty(text))
             return string.Empty;
 
-        var largestWordLength = GetLengthOfLargestWordInString(text);
+        var maxColumnSize = text.GetLengthOfLargestWordInString() + MIN_PADDING;
 
-        var lines = GetLines(text);
+        var lines = text.GetLines();
 
         var updatedLines = 
             lines
-                .Select(line => GetPaddedLine(line, largestWordLength))
+                .Select(line => line.GetPaddedLine(maxColumnSize))
                 .ToList();
 
         return string.Concat(updatedLines);
-    }
-
-    private static IEnumerable<string> GetLines(string text)
-    {
-        var lines =
-                text
-                    .Split(',', '.')
-                    .Where(s => !string.IsNullOrEmpty(s))
-                    .ToList()
-            ;
-        return lines;
-    }
-
-    private static string GetPaddedLine(string line, int largestWordLength)
-    {
-        var splitWords =
-            line
-                .Split('$')
-                .Where(s => !string.IsNullOrEmpty(s))
-                .ToList()
-                ;
-        
-        var paddedWords = 
-            splitWords
-                .Select(s => GetPaddedWord(s, largestWordLength))
-                .ToList()
-            ;
-        
-        paddedWords.Add("\n");
-        
-        return string.Concat(paddedWords);
-    }
-
-    private static string GetPaddedWord(string word, int largestWordLength)
-    {
-        var numberOfPaddingChars = (largestWordLength + 2) - word.Length;
-        var paddingWord = string.Concat(Enumerable.Repeat(' ', numberOfPaddingChars));
-        return word + paddingWord;
-    }
-    
-    private static int GetLengthOfLargestWordInString(string text)
-    {
-        return text
-            .Split('$')
-            .Select(s => s.Replace(",", string.Empty))
-            .Max(s => s.Length)
-            ;
     }
 }
